6a43f823be630cbbed470dbbfa0100704206ea3b
jasirpanghat <jazjasir7@gmail.com>
2025-07-09 02:15:39 +0530

    fix and reset notification for task section remaining

--- communications/utlis/notification_handler.py
+++ communications/utlis/notification_handler.py
@@ -2,23 +2,55 @@ from communications.models import Notifications
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 
-def notification_set(type, message, user=None):
-    print("id",user.id)
-    # Save the notification
-    save =  Notifications.objects.create(type=type, message=message, user=user)
+# def notification_set(type, message, user=None):
+#     print("id",user.id)
+#     # Save the notification
+#     save =  Notifications.objects.create(type=type, message=message, user=user)
 
-    # Send to WebSocket group
+#     # Send to WebSocket group
+#     channel_layer = get_channel_layer()
+#     user_id = user.user.id 
+#     print("user",user_id)
+#     async_to_sync( channel_layer.group_send)(
+#         f"user-{user_id}",  
+#         {
+#             "type": "user.notification", 
+#             "data": {
+#                 "id": save.id,
+#                 "type": type,
+#                 "message": message,
+#             }
+#         }
+#     )
+
+def save_notification(type, message, user=None):
+    """Save notification to database (sync operation)"""
+    return Notifications.objects.create(type=type, message=message, user=user)
+
+def send_websocket_notification(notification_id, type, message, user_id):
+    """Send notification via WebSocket (async operation)"""
     channel_layer = get_channel_layer()
-    user_id = user.user.id 
-    print("user",user_id)
-    async_to_sync( channel_layer.group_send)(
-        f"user-{user_id}",  
+    async_to_sync(channel_layer.group_send)(
+        f"user-{user_id}",
         {
-            "type": "user.notification", 
+            "type": "user.notification",
             "data": {
-                "id": save.id,
+                "id": notification_id,
                 "type": type,
                 "message": message,
             }
         }
     )
+
+
+def notification_set(type, message, user=None):
+    """Complete notification flow"""
+    print("id", user.id)
+    
+    # Save the notification
+    saved_notification = save_notification(type, message, user)
+
+    user_id = user.user.id 
+    print("user", user_id)
+    
+    send_websocket_notification(saved_notification.id, type, message, user_id)
