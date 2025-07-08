2669e71736a0e0fe75c8c287fd2639a88d4d1128
jasirpanghat <jazjasir7@gmail.com>
2025-07-08 18:46:23 +0530

    update pagination and user mail section

--- streamway/settings.py
+++ streamway/settings.py
@@ -141,8 +141,8 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:5173",
     "http://*.localhost:5173",
-    "www.streamway.solutions",
-    "api.streamway.solutions"
+    "https://api.streamway.solutions",
+    "https://www.streamway.solutions",
 ]
 
 
