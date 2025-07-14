074aafe604e67533f73d9d3abd7954d4eb6ef019
jasirpanghat <jazjasir7@gmail.com>
2025-07-14 07:23:18 +0530

    fix: update account response data

--- Customer/views.py
+++ Customer/views.py
@@ -117,7 +117,7 @@ class AccountsView(APIView):
         serializer = AccountsSerilalizer(account, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(AccountsViewSerializer(serializer.instance).data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         
